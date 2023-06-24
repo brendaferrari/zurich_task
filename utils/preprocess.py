cbfab6fbafe133eea88c9177bb1dc8718a00a27e
brendaferrari <brendafferrari@gmail.com>
2023-06-24 01:33:32 -0300

    first commit

---
+++ utils/preprocess.py
@@ -0,0 +1,229 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.impute import SimpleImputer
+from sklearn import linear_model
+from scipy.stats.mstats import winsorize
+
+class Preprocess:
+
+    def __init__(self, data):
+        self.data = data
+
+    def rm_NaN(self):
+
+        shape_before = self.data.shape
+        print(f'Before removing NaN values: {shape_before}\n')
+
+        self.data.replace([np.inf, -np.inf], np.nan, inplace=True)
+        header = list(self.data.columns.values)
+        A = self.data.isnull().any()
+        for fact in enumerate(A) :
+            if fact[1] == True :
+                print("NaN values found.")
+                col_Nan = header[fact[0]]
+                del self.data[col_Nan]
+
+        shape_after = self.data.shape
+        if shape_after == shape_before:
+            print("NaN values not found.")
+        print(f'After removing NaN values: {shape_after}')
+
+        return self.data
+
+    def rm_EmptyFeature(self) :
+
+        shape_before = self.data.shape
+        print(f'Before removing Empty Feature: {shape_before}\n')
+
+        zero_desc = []
+        header = list(self.data.columns.values)
+        for i in header :
+            flag = 0
+            column = self.data[i]
+            column = list(column)
+            stan = column[0]
+            for d in column :
+                if d != stan :
+                    flag = 1
+                    break
+            if flag == 0 :
+                zero_desc.append(i)
+        self.data = self.data.drop(zero_desc, axis=1)
+
+        shape_after = self.data.shape
+        if shape_after == shape_before:
+            print("Empty Feature values not found.")
+        print(f'After removing Empty Feature: {shape_after}')
+        
+        return self.data
+
+    def rm_HighCol(self, thresh):
+        """
+        Remove Columns with Colinearity Higher than a given threshold using a correlation matrix method
+        collinearity, in statistics, correlation between predictor variables (or independent variables), such that they express a 
+        linear relationship in a regression model. 
+        When predictor variables in the same regression model are correlated, they cannot independently predict the value of the dependent variable.
+        """
+        shape_before = self.data.shape
+        print(f'Before removing High Colinearity values: {shape_before}\n')
+
+        # Build a pearson correlation matrix
+        cor_matrix = self.data.corr(method ='pearson').abs()
+                    
+        # Convert the matrix in a upper triangular Matrix given the colinearity simmetry
+        upper_tri = cor_matrix.where(np.triu(np.ones(cor_matrix.shape),k=1).astype(bool))
+                    
+        # Write a list of columns with high colinearity to be dropped from original self.dataset
+        to_drop = [column for column in upper_tri.columns if any(upper_tri[column] > thresh)]
+                    
+        # Drop Selected columns and store self.dataframe
+        self.data = self.data.drop(to_drop, axis=1)
+        shape_after = self.data.shape
+        if shape_after == shape_before:
+            print("High Colinearity value not found.")
+        print(f'After removing High Colinearity values: {shape_after}')               
+        return self.data
+
+    def do_DataScaling(self, scaler):
+        
+        header = list(self.data.columns.values)
+        if scaler == 'normalization':
+            scaler_ = MinMaxScaler()
+        if scaler == 'standardization':
+            scaler_ = StandardScaler()
+        data_scaled = scaler_.fit_transform(self.data)
+        self.data = pd.DataFrame(data_scaled, columns=header)
+
+        return self.data
+
+    def get_LowVarianceColumns(self, thresh=0.0,
+                                autoremove=False):
+        """
+        Wrapper for sklearn VarianceThreshold for use on pandas self.dataframes.
+        """
+        print("Finding low-variance features.")
+        try:
+
+            allColumns = self.data.columns
+
+            X = self.data.values
+
+            vt = VarianceThreshold(threshold=thresh)
+            vt.fit(X)
+
+            feature_indices = vt.get_support(indices=True)
+            feature_names = [allColumns[idx]
+                                    for idx, _
+                                    in enumerate(allColumns)
+                                    if idx
+                                    in feature_indices]
+
+            # get the columns to be removed
+            removed_features = list(np.setdiff1d(allColumns,
+                                                        feature_names))
+            print("Found {0} low-variance columns."
+                        .format(len(removed_features)))
+
+            if autoremove:
+                print("Removing low-variance features.")
+                # remove the low-variance columns
+                X_removed = vt.transform(X)
+
+                print("Reassembling the self.dataframe (with low-variance "
+                            "features removed).")
+                # re-assemble the self.dataframe
+                self.data = pd.DataFrame(data=X_removed, columns=feature_names)
+                print("Succesfully removed low-variance columns.")
+
+                    # do not remove columns
+            else:
+                print("No changes have been made to the self.dataframe.")
+        
+        except Exception as e:
+            print(e)
+            print("Could not remove low-variance features. Something "
+                "went wrong.")
+            pass
+
+        return self.data
+
+    def outlier_removal_IQR(self):
+        """
+        Remove outliers using the inter quartile range technique
+        """
+        Q1 = self.data['target'].quantile(0.25)
+        Q3 = self.data['target'].quantile(0.75)
+        IQR = Q3 - Q1    #IQR is interquartile range. 
+
+        filter_ = (self.data['target'] >= Q1 - 1.5 * IQR) & (self.data['target'] <= Q3 + 1.5 *IQR)
+        self.data = self.data.loc[filter_]  
+
+        print(self.data.shape)
+        
+        return self.data
+    
+    def outlier_treatment(self, method='winsorize'):
+            """
+            Remove outliers using the inter quartile range technique
+            https://towardsdatascience.com/detecting-and-treating-outliers-in-python-part-3-dcb54abaf7b0#:~:text=robust)%20Mahalanobis%20distance.-,Imputation,based%20on%20the%20remaining%20data.
+            https://ndgigliotti.medium.com/trimming-vs-winsorizing-outliers-e5cae0bf22cb
+            """
+            Q1 = self.data['target'].quantile(0.25)
+            Q3 = self.data['target'].quantile(0.75)
+            IQR = Q3 - Q1    #IQR is interquartile range. 
+
+            inner_fence_ub = Q1 - 1.5 * IQR
+            inner_fence_lb = Q3 + 1.5 *IQR
+
+            outer_fence_ub = Q1 - 3 * IQR
+            outer_fence_lb = Q3 + 3 *IQR
+
+            if method == 'winsorize':
+                    print(outer_fence_lb)
+                    print(outer_fence_ub)
+                    print('89.5% quantile:   ', self.data['target'].quantile(0.895))       #10.75
+                    print('90% quantile:   ', self.data['target'].quantile(0.90))       #10.75
+                    print('92.5% quantile: ', self.data['target'].quantile(0.925))      #13.54
+                    print('95% quantile:   ', self.data['target'].quantile(0.95))       #15.79
+                    print('97.5% quantile: ', self.data['target'].quantile(0.975))      #23.93
+                    print('99% quantile:   ', self.data['target'].quantile(0.99))       #41.37
+                    print('99.9% quantile: ', self.data['target'].quantile(0.999))      #81.18
+
+                    data_w = self.data.copy(deep=True)
+
+                    #Winsorize on right-tail
+                    data_w['target_wins_89.5%'] = winsorize(self.data['target'], limits=(0, 0.105))
+            
+                    return data_w
+
+            if method == 'imputation':   
+                    outliers_prob = []
+                    outliers_poss = []
+                    for index, x in enumerate(self.data['target']):
+                            if x <= outer_fence_ub or x >= outer_fence_lb:
+                                    outliers_prob.append(index)
+                    for index, x in enumerate(self.data['target']):
+                            if x <= inner_fence_ub or x >= inner_fence_lb:
+                                    outliers_poss.append(index)
+
+                    for i in outliers_prob:
+                            self.data.at[i, 'target'] = None
+
+                    #Define imputer
+                    #imputer = IterativeImputer(estimator=LinearRegression(),
+                                            # n_nearest_features=None,
+                                            # imputation_order='ascending')
+                                            # #, sample_posterior=True)
+                    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+                    #Fit imputer and transform                          
+                    imputer.fit(self.data)
+                    df_imp_tf = imputer.transform(self.data)
+                    df_imp = pd.DataFrame(df_imp_tf, columns = self.data.columns)
+                    
+                    return df_imp
---
+++ utils/visualization.py
@@ -0,0 +1,174 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import DataStructs
+from rdkit.Chem import Draw
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.cm as cm
+import pandas as pd
+import numpy as np
+
+#Adapted from https://gist.github.com/iwatobipen/f8b0e8ea2c872e7ccf34ab472454ce6c#file-chemicalspace_lapjv-ipynb
+
+class Visualization:
+
+    def generate_images(self, smiles):
+        import base64
+        from rdkit.Chem.Draw import rdMolDraw2D
+
+        path_list = []
+
+        mol = [Chem.MolFromSmiles(s) for s in smiles]
+        c = 0
+        for m in mol:
+            print(f"Encoding molecule {c}: {smiles[c]}")
+            drawer = rdMolDraw2D.MolDraw2DCairo(500,500)
+            drawer.SetFontSize(6)
+            drawer.DrawMolecule(m)
+            drawer.FinishDrawing()
+            text = drawer.GetDrawingText()
+            imtext = base64.b64encode(text).decode('utf8')
+            im_url = "data:image/png;base64, " + imtext
+            path_list.append(im_url)
+            c += 1
+
+        return path_list
+
+    def chemical_space(self, smiles, name, activity):
+
+        # To name variables
+        mols = [Chem.MolFromSmiles(smi) for smi in smiles]
+        sampleidx = list(range(len(mols)))
+        samplemols = [mols[i] for i in sampleidx]
+
+        # Fingerprint used: lfcfp6
+        fps  = [AllChem.GetMorganFingerprintAsBitVect(m, 3, useFeatures=True, nBits=16384) for m in samplemols]
+
+        # To define array
+        def fp2arr(fp):
+            arr = np.zeros((0,))
+            DataStructs.ConvertToNumpyArray(fp,arr)
+            return arr
+
+        X = np.asarray([fp2arr(fp) for fp in fps])
+
+        # To calculate PCA and TSNE
+        data = PCA(n_components=94).fit_transform(X.astype(np.float32))
+        embeddings = TSNE(init='pca', random_state=794, verbose=2).fit_transform(data)
+        embeddings -= embeddings.min(axis=0)
+        embeddings /= embeddings.max(axis=0)
+
+        return embeddings
+
+    def plot(self, dataset, pca=False):
+
+        from jupyter_dash import JupyterDash
+        from dash import dcc, html, Input, Output, no_update
+        import plotly.graph_objects as go
+
+        path_list = self.generate_images(dataset.smiles)
+        dataset["path"] = path_list
+        if pca == True:
+            embeddings = self.chemical_space(dataset.smiles, dataset.id, dataset.prediction_reg)
+            data_x = embeddings[:,0]
+            data_y = embeddings[:,1]
+        else:
+            data_x = dataset.target
+            data_y = dataset.target
+
+        fig = go.Figure(data=[
+            go.Scatter(
+                x=data_x,
+                y=data_y,
+                mode="markers",
+                marker=dict(
+                    colorscale='viridis',
+                    color=dataset.target,
+                    #size=dataset.prediction_reg,
+                    colorbar={"title": "Molecular<br>Weight"},
+                    line={"color": "#444"},
+                    reversescale=True,
+                    sizeref=20,
+                    sizemode="diameter",
+                    opacity=0.8,
+                )
+            )
+        ])
+
+        # turn off native plotly.js hover effects - make sure to use
+        # hoverinfo="none" rather than "skip" which also halts events.
+        fig.update_traces(hoverinfo="none", hovertemplate=None, marker_size=20)
+
+        fig.update_layout(
+            xaxis=dict(title='chemical similarity'),
+            yaxis=dict(title='chemical similarity'),
+            plot_bgcolor='rgba(255,255,255,0.1)',
+            width=800, height=800
+        )
+
+        app = JupyterDash(__name__)
+
+        app.layout = html.Div([
+            dcc.Graph(id="graph", figure=fig, clear_on_unhover=True),
+            dcc.Tooltip(id="graph-tooltip"),
+        ])
+
+        @app.callback(
+            Output("graph-tooltip", "show"),
+            Output("graph-tooltip", "bbox"),
+            Output("graph-tooltip", "children"),
+            Input("graph", "hoverData"),
+        )
+        def display_hover(hoverData):
+            if hoverData is None:
+                return False, no_update, no_update
+
+            # demo only shows the first point, but other points may also be available
+            pt = hoverData["points"][0]
+            bbox = pt["bbox"]
+            num = pt["pointNumber"]
+
+            df_row = dataset.iloc[num]
+            img_src = df_row.path
+            #name = df_row.id
+            form = df_row.smiles
+            #pred= df_row.prediction_reg
+            # if len(desc) > 300: desc = desc[:100] + '...'
+
+            children = [
+                html.Div(children=[
+                    html.Img(src=img_src, style={"width": "100%"}),
+                    #html.H2(f"{name}", style={"color": "darkblue"}),
+                    html.P(f"{form}"),
+                    #html.P(f"{pred}"),
+                ],
+                style={'width': '300px', 'white-space': 'normal'})
+            ]
+
+            return True, bbox, children
+        
+        app.run_server(debug=True, mode='inline')
+
+    def features_plot(self, x_data, y_data, column):
+        df = x_data.join(y_data, lsuffix="_x", rsuffix="_y")
+        fig, axes = plt.subplots(1,len(df.columns.values)-1, sharey=True, figsize=(15,2))
+
+        for i, col in enumerate(df.columns.values[:-1]):
+            df.plot(x=[col], y=[column], kind="scatter", ax=axes[i])
+
+        plt.show()
+
+    def create_BoxHist(self, df):
+        # creating a figure composed of two matplotlib.Axes objects (ax_box and ax_hist)
+        f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})    
+
+        # assigning a graph to each ax
+        sns.boxplot(x=df, ax=ax_box)
+        sns.histplot(data=df, ax=ax_hist, bins=100)
+        ax_hist.set_yscale('log')
+
+        # Remove x axis name for the boxplot
+        ax_box.set(xlabel='')
+        plt.show()
